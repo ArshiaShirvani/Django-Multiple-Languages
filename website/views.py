f902f5b0cdfa381dff801a094e37e4abb8db1d8f
Arshia <arshiashirvani.f1385@gmail.com>
2025-01-19 22:37:10 +0330

    write urls and views for redirect in main page

--- website/views.py
+++ website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
+from django.utils.translation import get_language, activate, gettext
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+
+
